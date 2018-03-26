3c99973d9848d9cb19917b459d792e2289ccb204
Li Jing <jingli9111@gmail.com>
2018-03-26 12:06:35 -0400

    data chrop

--- band_data.py
+++ band_data.py
@@ -16,17 +16,30 @@ for i in f.keys():
 	if f[i]['bandgap_1'][0] == 0:
 		img = f[i]['epsilon_grid'][:]
 		img = np.array(img).reshape([32, 32])
-		plt.imshow(img)
 		print str(id) + ' A: w/o gap'
-		output_file_name = 'data/band/trainA/' + str(id) + '.jpg'
-		plt.savefig(output_file_name, format='jpg')
+
+		if id % 5 == 0:
+			output_file_name = 'data/band/trainA/' + str(id) + '.png'
+			plt.imsave(output_file_name, img, format="png", cmap="hot")
+		else:
+			output_file_name = 'data/band/testA/' + str(id) + '.png'
+			plt.imsave(output_file_name, img, format="png", cmap="hot")
+
+		
 	else:
 		img = f[i]['epsilon_grid'][:]
 		img = np.array(img).reshape([32, 32])
-		plt.imshow(img)
+
 		print str(id) + ' B: w gap'
-		output_file_name = 'data/band/trainB/' + str(id) + '.jpg'
-		plt.savefig(output_file_name, format='jpg')
+
+		if id % 5 == 0:
+			output_file_name = 'data/band/trainB/' + str(id) + '.png'
+			plt.imsave(output_file_name, img, format="png", cmap="hot")
+		else:
+			output_file_name = 'data/band/testB/' + str(id) + '.png'
+			plt.imsave(output_file_name, img, format="png", cmap="hot")
+
+
 	id += 1
 	plt.clf()
 
